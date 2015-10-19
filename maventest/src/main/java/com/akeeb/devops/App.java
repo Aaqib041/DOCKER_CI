aa3b78defd0b773d650d56e365f2a0e6f3979bbd
M1023764 <M1023764@A2ML15978.mindtree.com>
2015-10-19 18:06:48 +0530

    new line added-2

--- maventest/src/main/java/com/akeeb/devops/App.java
+++ maventest/src/main/java/com/akeeb/devops/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to DEVOPS" );
+        System.out.println( "Welcome to DEVOPS" );
     }
 }
